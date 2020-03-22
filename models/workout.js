ed73a1d1e7399f78a7a51dcd0a5651b7a4b3a6a3
kyle074613 <kylemj@uci.edu>
2020-03-21 22:14:39 -0700

    created models

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: Date,
+    excercises: [{
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number
+    }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
