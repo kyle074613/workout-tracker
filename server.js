fb87a40fe9bdb33b7e7429536dd7016e2c7d11c2
kyle074613 <kylemj@uci.edu>
2020-03-22 19:05:32 -0700

    finished server file, added route files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// app.use(require("./routes/htmlRoutes.js"));
+// app.use(require("./routes/apiRoutes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running. Go to http://localhost:${PORT}`)
+});
